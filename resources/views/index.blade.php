3204691334f121b209f0a472e5e61ab6e15d6d31
KellerMarika <marikakeller92@gmail.com>
2023-01-24 14:34:25 +0100

    migration created

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.public')
+
+@section('main')
+    <article>
+        <h1>MAIN</h1>
+    </article>
+
+
+{{dump($trains)}}
+
+    {{--    @include('partials.bunner', compact('a')) --}}
+@endsection
